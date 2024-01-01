f273d9663193243721adcac2a2fe6bfaa434bd4c
osawa-koki <koki@osawa.dev>
2024-01-01 15:18:21 +0900

    `dotnet format`

--- Program/Math.cs
+++ Program/Math.cs
@@ -1,15 +1,15 @@
-﻿namespace simple_project_cs
+namespace simple_project_cs
 {
-	public class Math
-	{
-		public static int add(int a, int b)
-		{
-			return a + b;
-		}
+    public class Math
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
 
-		public static int sub(int a, int b)
-		{
-			return a - b;
-		}
-	}
+        public static int sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
 }
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,12 +1,12 @@
-﻿namespace simple_project_cs
+namespace simple_project_cs
 {
-	public static class Program
-	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!!!");
-			Console.WriteLine("5 + 10 = " + Math.add(5, 10));
-			Console.WriteLine("5 - 10 = " + Math.sub(5, 10));
-		}
-	}
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("5 + 10 = " + Math.add(5, 10));
+            Console.WriteLine("5 - 10 = " + Math.sub(5, 10));
+        }
+    }
 }
